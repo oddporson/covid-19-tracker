ee2e6b0eec3e456aac62e8f0c1f5d7263f6dfc71
Porson <porsonlee@gmail.com>
2020-04-19 18:21:03 -0700

    real data is now showing

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -25,23 +25,37 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                 seperator=","
               />
             </Typography>
-            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
             <Typography variant='body2'>Number of active cases of Covid-19</Typography>
           </CardContent>
         </Grid>
         <Grid item component={Card}>
           <CardContent>
             <Typography color='textSecondary' gutterBottom>Recovered</Typography>
-            <Typography variant="h5">REAL DATA</Typography>
-            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="h5">
+            <CountUp 
+                start={0}
+                end={recovered.value}
+                duration={2.5}
+                seperator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
             <Typography variant='body2'>Number of recoveries from Covid-19</Typography>
           </CardContent>
         </Grid>
         <Grid item component={Card}>
           <CardContent>
             <Typography color='textSecondary' gutterBottom>Deaths</Typography>
-            <Typography variant="h5">REAL DATA</Typography>
-            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="h5">
+            <CountUp 
+                start={0}
+                end={deaths.value}
+                duration={2.5}
+                seperator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
             <Typography variant='body2'>Number of deaths caused by Covid-19</Typography>
           </CardContent>
         </Grid>
