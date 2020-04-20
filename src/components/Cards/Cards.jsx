173a49ad254e624bf2c6a4e2436dda8f884308e0
Porson <porsonlee@gmail.com>
2020-04-19 18:15:32 -0700

    created card template and pass the data temporary

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
-const Cards = (props) => {
-  console.log(props);
+import styles from './Cards.module.css'
+
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  console.log(confirmed);
+  if(!confirmed) {
+    return 'Loading...';
+  }
 
   return (
-    <h1>Cards</h1>
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color='textSecondary' gutterBottom>Infected</Typography>
+            <Typography variant="h5">{confirmed.value}</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant='body2'>Number of active cases of Covid-19</Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color='textSecondary' gutterBottom>Recovered</Typography>
+            <Typography variant="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant='body2'>Number of recoveries from Covid-19</Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color='textSecondary' gutterBottom>Deaths</Typography>
+            <Typography variant="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant='body2'>Number of deaths caused by Covid-19</Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
   )
 }
 
