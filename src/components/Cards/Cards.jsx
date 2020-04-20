0f971f37856cbc7a6d8d366302b8da1b5f0634d9
Porson <porsonlee@gmail.com>
2020-04-19 18:17:29 -0700

    used CountUp that runs the number from 0 to confirmed case when it is webpage is reloaded

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
+
 
 import styles from './Cards.module.css'
 
@@ -15,7 +17,14 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
         <Grid item component={Card}>
           <CardContent>
             <Typography color='textSecondary' gutterBottom>Infected</Typography>
-            <Typography variant="h5">{confirmed.value}</Typography>
+            <Typography variant="h5">
+              <CountUp 
+                start={0}
+                end={confirmed.value}
+                duration={2.5}
+                seperator=","
+              />
+            </Typography>
             <Typography color="textSecondary">REAL DATE</Typography>
             <Typography variant='body2'>Number of active cases of Covid-19</Typography>
           </CardContent>
