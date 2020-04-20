75fdbba2cdaa610e8793566cd15cda2b243d0a33
Porson <porsonlee@gmail.com>
2020-04-19 18:03:43 -0700

    passing down props

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import { fetchData } from './api';
 
 
 class App extends React.Component { 
+  state = { 
+    data: {},
+  }
+
 
   async componentDidMount() {
-    const data = await fetchData();
-    console.log(data); 
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
   }
   render() {
+    const { data } = this.state
     return (
       <div className={styles.container}>
-        <Cards /> 
+        <Cards data={data} /> 
         <CountryPicker />
         <Chart /> 
       </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-const Cards = () => {
+const Cards = (props) => {
+  console.log(props);
+
   return (
     <h1>Cards</h1>
   )
